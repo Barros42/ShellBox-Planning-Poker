ee60f9a4707e6c267fce3e9de03081baeebf16fb
Barros <matheus.fagionato@raizen.com>
2021-02-05 18:14:28 -0300

    Added AppLogger

--- api/helpers/appLogger.js
+++ api/helpers/appLogger.js
@@ -2,7 +2,8 @@ import AppConfig from '../config/AppConfig.js'
 
 const appLogger = () => {
   console.clear()
-  console.log(`${AppConfig.API_NAME} API @ Running on port ${AppConfig.API_PORT}`)
+  console.log(`${AppConfig.API_NAME} API @ Running`)
+  console.log(`Config :: APP PORT @ ${AppConfig.API_PORT}`)
   console.log(`Config :: APP CORS @ ${AppConfig.USE_CORS}`)
   console.log(`----------------------------------------------------------------`)
 }
