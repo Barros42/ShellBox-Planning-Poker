d90a2d47083099a8419b921cbc555c5a9b6345d6
Barros <matheus.fagionato@raizen.com>
2021-04-29 18:16:57 -0300

    Bugfix Show Votes

--- app/src/Components/UserItem/index.tsx
+++ app/src/Components/UserItem/index.tsx
@@ -12,12 +12,13 @@ const UserItem = (props: IUserItem) => {
     const showVotesClass = (props.showVotes) ? 'voted' : ''
     const userVote = (props.vote) ? props.vote : '~'
     const containVotesClass = (props.vote) ? 'contain-votes' : ''
-    const userItemTitle = (userVote !== '~') ? `${props.name} voted ${userVote}` : `${props.name} didn't vote`
+    const userVoteText = (props.showVotes) ? userVote : '[XX]'
+    const userItemTitle = (userVote !== '~') ? `${props.name} voted ${userVoteText}` : `${props.name} didn't vote`
 
     return(
         <div className={`user-item ${containVotesClass} ${showVotesClass}`} title={`${props.name}`}>
             <div className='user-item-name'>{props.name}</div>
-            <div className='user-item-nota' title={userItemTitle}>{userVote}</div>
+            <div className='user-item-nota' title={userItemTitle}>{userVoteText}</div>
         </div>
     )
 }
