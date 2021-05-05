80fc963eeaae00c12ea4f393a5fef60527c4b69a
Jovino Esteban Guerrero Toledo <CS333972@MinhaTI.com.br>
2021-05-05 16:33:57 -0300

    Contribution - Spanish dictionary creation

--- app/src/Core/Dictionary/index.ts
+++ app/src/Core/Dictionary/index.ts
@@ -2,12 +2,15 @@ import Navigator, { NavigatorLanguage } from "Helpers/Navigator";
 import EnglishDictionary from "./EnglishDictionary"
 import IDictionary from "./IDictionary";
 import PortugueseDictionary from "./PortugueseDictionary";
+import SpanishDictionary from "./SpanishDictionaryy";
 
 class DictionaryFactory {
   static getDictionary(language: NavigatorLanguage): IDictionary {
     switch (language) {
       case NavigatorLanguage.PT_BR:
         return new PortugueseDictionary()
+      case NavigatorLanguage.ES_AR:
+        return new SpanishDictionary()
       case NavigatorLanguage.EN_US:
         return new EnglishDictionary()
       default:
