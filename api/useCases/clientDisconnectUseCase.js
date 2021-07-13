0a215a5dcb18e1ac275470a2e690f190a0d6c4b0
Barros <matheus.fagionato@raizen.com>
2021-07-13 10:37:16 -0300

    Fix @ Reset Story Name on Empry Room

--- api/useCases/clientDisconnectUseCase.js
+++ api/useCases/clientDisconnectUseCase.js
@@ -1,20 +1,23 @@
 import RoomRepository from '../external/repositories/room.repository.js'
+import initializeRoom from '../helpers/initializeRoom.js'
 
 const ClientDisconnectUseCase = {
 
     run: (data) => {
 
-        const userAdded = RoomRepository.getUser(data.room, data.client.id)
+        const userExists = RoomRepository.getUser(data.room, data.client.id)
 
-        if(userAdded){
+        if(userExists){
             RoomRepository.removeUser(data.room, data.client)
         }
 
         const usersFromFroom = RoomRepository.getAllUsersFromRoom(data.room)
-        const hasUsersInRoom = (!usersFromFroom || !usersFromFroom.length)
+        const noUsersInRoom = (!usersFromFroom || !usersFromFroom.length)
 
-        if(hasUsersInRoom){
+        if(noUsersInRoom){
+            const emptyRoom = initializeRoom()
             RoomRepository.changeRoomVoteVisibility(data.room, false)
+            RoomRepository.updateRoomStory(data.room, emptyRoom.story)
         }
         
         return RoomRepository.getRoom(data.room)
